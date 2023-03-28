3cabdd40bd9b9f6a8fd899cd5bf0fa79ed26b425
DeepaliShar <212deepali.sharma@gmail.com>
2023-03-28 14:55:23 +0530

    done

---
+++ BinaryToDecimal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class BinaryToDecimal {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number");
+    int n=sc.nextInt();
+    while(n!=0){
+        
+    }
+}
---
+++ hcf.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class hcf{
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter 1st Number");
+    int n1=sc.nextInt();
+    System.out.println("Enter 2nd Number");
+    int n2=sc.nextInt();
+    int hcf=1;
+      for(int i=1;i<=n1&&i<=n2;i++){
+        if(n1%i==0 && n2%i==0)
+        hcf=i;
+      }
+      System.out.println(hcf);
+  }
+}
